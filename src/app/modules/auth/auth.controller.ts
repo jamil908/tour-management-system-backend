d6f93f685721314c67c944f6531f4d5fb888f868
jamil9080 <md3711451@gmail.com>
2026-01-30 18:47:21 +0600

    work in progress: auth controller update

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express"
 import { catchAsync } from "../../utils/catchAsync"
 import { UserServices } from "../user/user.service"
 import { sendResponse } from "../../utils/sendResponse"
 import httpStatus from 'http-status-codes'
 import { AuthService } from "./auth.service"
-const credentialsLogin = catchAsync(async (req : Request ,res : Response, next : NextFunctionon)=>{
+const credentialsLogin = catchAsync(async (req : Request ,res : Response, next : NextFunction)=>{
     // const user = await UserServices.createUser(req.body)
     const logginInfo = await AuthService.credentialsLogin(req.body)
        sendResponse(res,{
